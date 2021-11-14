15aac8fe45cb2c56d6915a468f85bb190f24df74
gifnksm <makoto.nksm+github@gmail.com>
2021-11-14 13:30:27 +0900

    3章 順序回路

--- misc/hdl.mk
+++ misc/hdl.mk
@@ -7,7 +7,9 @@ MAKEFLAGS += --no-builtin-rules
 
 DIRNAME=$(notdir $(CURDIR))
 HDL=$(wildcard *.hdl)
-TARGET_DIR=../target/nand2tetris/projects/$(DIRNAME)
+GIT_CDUP=$(shell git rev-parse --show-cdup)
+GIT_PREFIX=$(patsubst %/,%,$(shell git rev-parse --show-prefix))
+TARGET_DIR=$(GIT_CDUP)/target/nand2tetris/projects/$(GIT_PREFIX)
 
 all:
 .PHONY: all
@@ -19,13 +21,13 @@ test: $(patsubst %.hdl,test-%,$(HDL))
 .PHONY: test
 
 test-%: $(TARGET_DIR)/%.ok
-	@echo "[TEST $(DIRNAME)/$(notdir $(basename $<))] OK"
+	@echo "[TEST $(GIT_PREFIX)/$(notdir $(basename $<))] OK"
 .PHONY: test-%
 .PRECIOUS: $(TARGET_DIR)/%.ok
 
 $(TARGET_DIR)/%.ok: $(TARGET_DIR)/%.hdl $(addprefix $(TARGET_DIR)/,$(HDL))
-	@echo "[TEST $(DIRNAME)/$(notdir $(basename $<))] running ..."
-	../tools/HardwareSimulator $(TARGET_DIR)/$(notdir $(basename $@)).tst
+	@echo "[TEST $(GIT_PREFIX)/$(notdir $(basename $<))] running ..."
+	$(GIT_CDUP)tools/HardwareSimulator $(TARGET_DIR)/$(notdir $(basename $@)).tst
 	@touch $@
 
 $(TARGET_DIR)/%.hdl: %.hdl
